207f6c53eac4980059a7942bea73c9ff9c503c13
Abiola Adeye <adeye.abiola@gmail.com>
2021-11-01 15:25:41 +0100

    comment added to sgd

--- scripts/helpers/stochastic_gradient_descent.py
+++ scripts/helpers/stochastic_gradient_descent.py
@@ -4,12 +4,14 @@ from helpers.costs import *
 from helpers.proj1_helpers import batch_iter
 
 def compute_stoch_gradient(y, tx, w):
+    """Computes one step of stochastic gradient descent"""
         e = y - np.dot(tx,w)
         N = y.shape[0]
         return (-1/N)* np.dot(np.transpose(tx),e)
 
 
 def stochastic_gradient_descent(y, tx, initial_w, max_iters, gamma,  batch_size = 1 ):
+    """Stochastic Gradient Descent for the given parameters and batch_size, returns optimal estimates for the weight vector"
         w = initial_w
         for n_iter in range(max_iters):
             for minibatch_y, minibatch_tx in batch_iter(y, tx, batch_size):
